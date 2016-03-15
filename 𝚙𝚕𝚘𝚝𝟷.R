7c074ed925fb50d517a102df762dff5f7c268e73
eliana <eliana.marinari@hcuge.ch>
2016-03-15 11:19:23 +0100

    assignment

---
+++ 𝚙𝚕𝚘𝚝𝟷.R
@@ -0,0 +1,27 @@
+url<-"http://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip"
+
+download.file(url,destfile = "data.zip")
+
+unzip("data.zip")
+
+data<-read.table("household_power_consumption.txt",header = TRUE, sep= ";")
+
+names(data)
+
+lapply(data, class)
+
+data$DateTime<-paste(data$Date, data$Time)
+
+data$DateTime<-strptime(data$DateTime, "%d/%m/%Y %H:%M:%S")
+
+start<-which(data$DateTime==strptime("2007-02-01", "%Y-%m-%d"))
+
+end<-which(data$DateTime==strptime("2007-02-02 23:59:00", "%Y-%m-%d %H:%M:%S"))
+
+data2<-data[start:end,]
+
+png(file="plot1.png")
+
+hist(as.numeric(as.character(data2$Global_active_power)),main="Global Active Power",xlab="Global Active Power (kilowatts)", col="red")
+
+dev.off()
